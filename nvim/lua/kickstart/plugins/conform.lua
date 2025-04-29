d33483ec3a18e0a77366191c9d5f5169f682db04
StephenCouturier <106610171+StephenCouturier@users.noreply.github.com>
2025-04-29 15:02:32 -0400

    add html editing

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
+        html = { 'prettierd', 'prettier', stop_after_first = true },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
