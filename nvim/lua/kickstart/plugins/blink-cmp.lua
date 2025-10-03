3caadb391a738ab63a708bf8df5001801a1c82af
StephenCouturier <106610171+StephenCouturier@users.noreply.github.com>
2025-10-03 09:34:22 -0400

    updates

--- nvim/lua/kickstart/plugins/blink-cmp.lua
+++ nvim/lua/kickstart/plugins/blink-cmp.lua
@@ -57,7 +57,10 @@ return {
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'enter',
+        preset = 'default',
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
