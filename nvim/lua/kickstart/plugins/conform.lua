dc45d30e4508793e602bcdf874e767c95df101ed
StephenCouturier <106610171+StephenCouturier@users.noreply.github.com>
2025-10-22 15:21:17 -0400

    fix tsx files

--- nvim/lua/kickstart/plugins/conform.lua
+++ nvim/lua/kickstart/plugins/conform.lua
@@ -29,6 +29,7 @@ return {
         html = { 'prettierd', 'prettier', stop_after_first = true },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
